406187c4177ed9ec9caf4afe66ccfa2802214ca6
S-O <s@example.com>
2023-12-22 13:11:10 +0530

    start game boradcast when 2 players are ready and define 2nd player as referee

--- sockets.js
+++ sockets.js
@@ -1,6 +1,17 @@
+let readyPlayerCount = 0;
+
 function listen(io) {
   io.on("connection", (socket) => {
-    console.log("a user connected", socket.id);
+    console.log("Player connected", socket.id);
+    
+    socket.on("ready", () => {
+      console.log("Player ready", socket.id);
+      readyPlayerCount++;
+
+      if (readyPlayerCount % 2 === 0) {
+        io.emit("startGame", socket.id); //sending 2nd player as referee to all connected clients
+      }
+    });
   });
 }
 
